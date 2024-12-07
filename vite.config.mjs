73bfe312afd1b3a76558cb8ceeeef4b1e5032321
BrennanTibbetts <brennanttibbetts@gmail.com>
2024-12-07 18:08:33 -0500

    Vercel Rewrites working as expected

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,24 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import glsl from "vite-plugin-glsl"
+import glsl from "vite-plugin-glsl";
+import rewriteAll from "vite-plugin-rewrite-all"; // Plugin for rewriting
+
+const boxleURL = "boxle.vercel.app"; // Replace with actual Boxle URL
+const portfolioURL = "brennant-portfolio.vercel.app"; // Replace with actual Portfolio URL
 
 // This is required for Vite to work correctly with CodeSandbox
 const server = process.env.APP_ENV === "sandbox" ? { hmr: { clientPort: 443 } } : {};
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
-  server: server,
+  server: {
+    ...server,
+  },
   resolve: {
     alias: {
       "@src": resolve(__dirname, "./src"),
     },
   },
-  plugins: [
-    react(),
-    glsl()
-  ],
+  plugins: [react(), glsl(), rewriteAll()],
 });
